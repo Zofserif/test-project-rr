7d7e414a5420ec5694a607fbadd87ad92c0e9dd4
zofserif <vallarta.troy@gmail.com>
2024-07-31 09:53:15 +0800

    Connecting db to ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,15 @@
 import { db } from "~/server/db";
 
+export const dynamic = "force-dynamic";
+
 export default async function HomePage() {
-  // let posts = [];
-  // try {
-  //   posts = await db.query.posts.findMany();
-  // } catch (error) {
-  //   console.error("Failed to fetch posts:", error);
-  //   return (
-  //     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-  //       <div>Error loading posts. Please try again later.</div>
-  //     </main>
-  //   );
-  // }
+  const posts = await db.query.posts.findMany();
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      {/* {posts.map((post) => (
+    <main>
+      {posts.map((post) => (
         <div key={post.id}>{post.name}</div>
-      ))} */}
+      ))}
     </main>
   );
 }
