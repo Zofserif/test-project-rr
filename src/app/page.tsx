5fc77aea46203cd95ed6c0bc5e35fea6f7a378f1
zofserif <vallarta.troy@gmail.com>
2024-08-01 12:20:27 +0800

    Using Next Image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { SignedIn, SignedOut } from "@clerk/nextjs";
 import { TopNav } from "./_components/topnav";
 import { getMyImages } from "~/server/queries";
+import Image from "next/image";
 export const dynamic = "force-dynamic";
 
 async function Images() {
@@ -10,7 +11,14 @@ async function Images() {
     <div className="flex flex-wrap justify-center gap-4 p-4">
       {images.map((image) => (
         <div key={image.id} className="flex h-48 w-48 flex-col">
-          <img src={image.url} className="flex flex-wrap gap-4" />
+          <Image
+            src={image.url}
+            style={{ objectFit: "contain" }}
+            width={480}
+            height={480}
+            alt={image.name}
+            className="flex flex-wrap gap-4"
+          />
           <div>{image.name}</div>
         </div>
       ))}
